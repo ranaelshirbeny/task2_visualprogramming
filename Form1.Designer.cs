80c702a0d95c5bc4748b3505e02af237017e6fd9
Rana Elsayed Elshirbeny <150361241+ranaelshirbeny@users.noreply.github.com>
2024-11-03 20:52:12 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.previous_button = new System.Windows.Forms.Button();
+            this.next_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(722, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // previous_button
+            // 
+            this.previous_button.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.previous_button.Location = new System.Drawing.Point(223, 394);
+            this.previous_button.Name = "previous_button";
+            this.previous_button.Size = new System.Drawing.Size(75, 23);
+            this.previous_button.TabIndex = 1;
+            this.previous_button.Text = "Previous";
+            this.previous_button.UseVisualStyleBackColor = false;
+            this.previous_button.Click += new System.EventHandler(this.previous_button_Click);
+            // 
+            // next_button
+            // 
+            this.next_button.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.next_button.Location = new System.Drawing.Point(472, 394);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(75, 23);
+            this.next_button.TabIndex = 2;
+            this.next_button.Text = "Next";
+            this.next_button.UseVisualStyleBackColor = false;
+            this.next_button.Click += new System.EventHandler(this.next_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.next_button);
+            this.Controls.Add(this.previous_button);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Image SlideShow";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button previous_button;
+        private System.Windows.Forms.Button next_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+      
+        private List<string> imagepaths;
+        private int currentIndex = 2;
+        public Form1()
+        {
+            InitializeComponent();
+            imagepaths = new List<string>
+          {
+                @"E:\\Myproject_HTML\30.jpg",
+               @"E:\\Myproject_HTML\40.jpg",
+               @"E:\\Myproject_HTML\50.jpg",
+               @"E:\\Myproject_HTML\60.jpg",
+               @"E:\\Myproject_HTML\70.jpg",
+               @"E:\\Myproject_HTML\80.jpg",
+               @"E:\\Myproject_HTML\90.jpg"
+
+
+          };
+            DisplayImage();
+        }
+
+    
+
+        private void DisplayImage()
+        {
+            
+              if(imagepaths.Count > 0)
+            {
+                pictureBox1.ImageLocation = imagepaths[currentIndex];
+                
+            }
+            
+        }
+
+        private void previous_button_Click(object sender, EventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                DisplayImage();
+                }
+            else
+            {
+                MessageBox.Show("this the first pictue");
+            }
+        }
+
+        private void next_button_Click(object sender, EventArgs e)
+        {
+            if (currentIndex < imagepaths.Count - 1)
+            {
+                currentIndex++;
+                DisplayImage();
+            }
+        }
+        
+    }
+}
